ca72c015c6f79ac9f6be6848fc0dc7e2d7fe71a6
Shubham Dwivedi <dwivedishubham953@gmail.com>
2020-04-06 01:04:21 +0530

    ticket book test modified

--- convoapis/tests.py
+++ convoapis/tests.py
@@ -97,4 +97,4 @@ class ApiTestCases(TestCase):
         response = self.client.post(
             reverse('movieapis:book_ticket'), payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        # self.assertEqual(response.data, BOoK_TICKET_SERIALIZER_ERROR)
+        self.assertEqual(response.data, BOoK_TICKET_SERIALIZER_ERROR)
